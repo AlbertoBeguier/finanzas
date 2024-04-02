52478fc6aa1bf2c9e1237e88066d15b96a131468
Alberto Beguier <aabeguier@hotmail.com>
2024-04-02 20:41:13 -0300

    v7

--- src/App.jsx
+++ src/App.jsx
@@ -80,7 +80,7 @@ function App() {
           </div>
         </div>
       </div>
-
+      <br />
       <div className="container">
         <div className="p-3 border bg-light rounded ">
           <div className="row justify-content-center">
